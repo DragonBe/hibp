9d1bff908eac74bd8cac0604dcc9a6e3ce442f30
Michelangelo van Dam <dragonbe+github@gmail.com>
2019-04-07 23:27:29 +0200

    Improving example code
    We have improved the example code with additional comments and usage of namespaces.

--- examples/hibp.php
+++ examples/hibp.php
@@ -1,8 +1,22 @@
 <?php
 
+namespace Hibp;
+
+use Dragonbe\Hibp\Hibp;
+use Dragonbe\Hibp\HibpFactory;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$hibp = \Dragonbe\Hibp\HibpFactory::create();
+/*
+ * This example shows how to quickly get started
+ * to make a call to the Have I been pwned API service.
+ */
+
+/**
+ * @var Hibp
+ */
+$hibp = HibpFactory::create();
+
 echo 'Password "password": ' . ($hibp->isPwnedPassword('password') ? 'Pwned' : 'OK') . PHP_EOL;
 echo 'Password "NVt3MpvQ": ' . ($hibp->isPwnedPassword('NVt3MpvQ') ? 'Pwned' : 'OK') . PHP_EOL;
 
